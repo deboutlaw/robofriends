c0f80d405d5710977fd49d027c925100e29ff82c
Deb Outlaw <deborahaoutlaw@gmail.com>
2020-10-31 20:42:35 -0400

    finished project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import {robots} from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import ErrorBoundry from './ErrorBoundry';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -11,6 +14,12 @@ class App extends Component {
       searchfield: ''
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+     .then(response => response.json())
+     .then(users => this.setState({robots: users}));
+    }
 
   onSearchChange = (event) => {
     this.setState({searchfield: event.target.value})
@@ -22,16 +31,19 @@ class App extends Component {
   render() {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    });
-    return(
-        <div className= 'tc'>
-          <h1>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
-          <CardList robots={filteredRobots}/>
-
-        </div>
-    );
-  
+    })
+    if (this.state.robots.length === 0) {
+      return <h1 className ='tc'>Loading</h1>
+    } else {
+      return (
+          <div className= 'tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <Scroll><ErrorBoundry><CardList robots={filteredRobots}/></ErrorBoundry></Scroll>
+          </div>
+         );
+    
+      }
     }
   }
 
